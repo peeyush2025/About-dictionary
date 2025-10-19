cf7b8eee30ec352c12d40c6f4c9841c0a492a672
peeyush2025 <sudhakarln82@gmail.com>
2025-10-19 19:57:52 +0530

    Add files via upload

---
+++ activity3.py
@@ -0,0 +1,5 @@
+SPCMcountry_code={'India':'0091','australia':'oo25','nepal':'00977'}
+print("Country code for India-")
+print(country_code.get('India','Not Found'))
+print("Country code for Japan-")
+print(country_code.get('Japan','Not Found'))
